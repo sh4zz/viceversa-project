9fcc0859131afcd6e159708a69245588a2ff3065
Melnychenko Volodymyr <frenkruberuk@gmail.com>
2021-05-10 21:21:31 +0300

    Add new func reverse

--- viceversa/views.py
+++ viceversa/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html')
+
+def reverse(request):
+	test_get_text = request.GET['usertext']
+	print(test_get_text)
+	return render(request, 'reverse.html')
